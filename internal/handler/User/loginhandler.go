571b07e97fcb1fbe7399bf7084b72906cc0eaf46
jonysahhh <2297646354@qq.com>
2024-08-05 22:51:33 +0800

    init

---
+++ internal/handler/User/loginhandler.go
@@ -0,0 +1,28 @@
+package User
+
+import (
+	"net/http"
+
+	"github.com/zeromicro/go-zero/rest/httpx"
+	"uscOPS/internal/logic/User"
+	"uscOPS/internal/svc"
+	"uscOPS/internal/types"
+)
+
+func LoginHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.LoginReq
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+			return
+		}
+
+		l := User.NewLoginLogic(r.Context(), svcCtx)
+		resp, err := l.Login(&req)
+		if err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+		} else {
+			httpx.OkJsonCtx(r.Context(), w, resp)
+		}
+	}
+}
---
+++ internal/svc/servicecontext.go
@@ -0,0 +1,15 @@
+package svc
+
+import (
+	"uscOPS/internal/config"
+)
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
